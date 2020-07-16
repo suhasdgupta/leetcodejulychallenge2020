dd22a2980ad3b9f992211d3229d100b29aab3250
Suhaas D <suhasdgupta55555@gmail.com>
2020-07-16 15:55:33 +0530

    Update PowXN.java

--- PowXN.java
+++ PowXN.java
@@ -1,11 +1,12 @@
 class PowXN {
     public double myPow(double x, int n) {
-        if(n == 0)
-            return 1;
-        if(n<0){
-            n = -n;
-            x = 1/x;
+        double ans = 1;
+        long absN = Math.abs((long)n);
+        while(absN > 0) {
+            if((absN&1)==1) ans *= x;
+            absN >>= 1;
+            x *= x;
         }
-        return (n%2 == 0) ? Math.pow(x*x, n/2) : x*Math.pow(x*x, n/2);
+        return n < 0 ?  1/ans : ans;
     }
 }
