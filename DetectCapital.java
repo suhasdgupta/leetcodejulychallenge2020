b170cb491a97e08dcf18bb117542ca48813076e1
Suhaas D <suhasdgupta55555@gmail.com>
2020-08-01 19:55:19 +0530

    Delete DetectCapital.java

--- DetectCapital.java
+++
@@ -1,7 +0,0 @@
-class DetectCapital {
-    public boolean detectCapitalUse(String word) {
-        int cnt = 0;
-        for(char c: word.toCharArray()) if('Z' - c >= 0) cnt++;
-        return ((cnt==0 || cnt==word.length()) || (cnt==1 && 'Z' - word.charAt(0)>=0));
-    }
-}
